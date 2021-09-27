98b8c3782ecf201cd6a4a2b8fbe4364dd11905a4
cck_sam <chanyuksingsam@gmail.com>
2021-09-27 13:10:57 +0800

    02/33 completed

---
+++ php-basic-tutorial/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo 'PHP入門教學'?></title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <?php
+        echo "<h1>Hello World in PHP </h1> <br>" ;
+        print 'hello world in php <br>';
+        echo 'echo比print快少少 </br>';
+        echo date();
+        ?>
+</body>
+</html>
